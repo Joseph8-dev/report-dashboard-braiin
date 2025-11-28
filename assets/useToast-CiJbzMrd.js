d996ad53bd30bfcd257c036c28c6babaef340582
Joseph Pérez <arcaneum13@gmail.com>
2025-11-28 15:39:03 -0400

    Updates

--- assets/useToast-CiJbzMrd.js
+++
@@ -1,2 +0,0 @@
-import{c as a,g as l,aj as r,ak as u,al as c,am as i}from"./index-DwbNooJy.js";const p=()=>{const e=l();return a(()=>{var o;return((o=r())==null?void 0:o._context)||(e==null?void 0:e.appContext)})},v=()=>{const e=p(),o=[],s=t=>{const n=i(t,e.value);return n&&o.push(n),n};return{init:t=>s(t),notify:s,close:t=>c(t),closeAll:(t=!1)=>u(t?void 0:e.value),closeAllCreatedInThisHook:()=>{o.forEach(t=>c(t))}}};export{v as u};
-//# sourceMappingURL=useToast-CiJbzMrd.js.map
