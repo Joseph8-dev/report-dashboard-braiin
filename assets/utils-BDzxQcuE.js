a069d975d3de615c4311fd6c3daacc308a15b658
Joseph Pérez <arcaneum13@gmail.com>
2025-12-15 22:41:45 -0400

    Updates

--- assets/utils-BDzxQcuE.js
+++
@@ -1,2 +0,0 @@
-const t=(e=0)=>new Promise(r=>setTimeout(r,e)),s={email:e=>/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(e)||"Por favor, ingresa una dirección de correo electrónico válida",required:e=>!!e||"Este campo es obligatorio"};export{t as s,s as v};
-//# sourceMappingURL=utils-BDzxQcuE.js.map
